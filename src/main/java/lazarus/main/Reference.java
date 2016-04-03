949474ba1800a8dd0c028dd8bee36397fac42f95
Samalot <samalotplays@gmail.com>
2016-04-04 00:59:46 +0100

    Setup very basic files, got rid of example files.

---
+++ src/main/java/lazarus/main/Reference.java
@@ -0,0 +1,9 @@
+/*Imports*/
+package lazarus.main;
+
+/*Configs*/
+public class Reference {
+	public static final String MOD_ID = "lazarus";
+	public static final String MOD_NAME = "Lazarus";
+	public static final String VERSION = "0.1";
+}
